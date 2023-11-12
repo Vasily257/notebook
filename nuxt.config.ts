4e2f5bdd2285aa5315431ed0f49edb239003c107
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-12 12:02:46 +0500

    build: add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [["@nuxtjs/eslint-module", { lintOnStart: false }]],
+  modules: [['@nuxtjs/eslint-module', { lintOnStart: false }]],
+  css: ['./assets/css/main.css'],
 });
