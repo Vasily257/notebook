bbb9f8b29c3cf4940904ec709b68f30ee83150f7
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-13 15:18:15 +0500

    feat: add the TheHeader component

---
+++ components/TheHeader.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+/** Настройки основной иконки */
+const mainIcon = computed(() => {
+  const mainIcon = {
+    name: 'contacts',
+    width: 31,
+    height: 24,
+  };
+
+  return mainIcon;
+});
+</script>
+
+<template>
+  <header class="header">
+    <BaseIcon :icon-name="mainIcon.name" :width="mainIcon.width" :height="mainIcon.height" />
+    <span class="header__text header__text--uppercase">Книга контактов</span>
+    <BaseIcon v-if="true" class="header__close-icon" icon-name="close" :width="24" :height="24" />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  column-gap: 4px;
+  flex-flow: row nowrap;
+  width: 100%;
+  height: 40px;
+  padding: 0 12px;
+  background-color: #282828;
+
+  &__text {
+    color: #dddddd;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 1;
+
+    &--uppercase {
+      text-transform: uppercase;
+    }
+  }
+
+  &__close-icon {
+    margin-left: auto;
+  }
+}
+</style>
