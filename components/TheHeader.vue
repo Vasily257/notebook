b178201350e24ecd29fc8f96a198ea050c5c2dd8
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-14 14:09:57 +0500

    feat(TheHeader): configure for "new" and "edit" pages

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,47 +1,94 @@
 <script setup lang="ts">
 import useHeader from '@/composable/header';
 
-const { isCloseShown, mainIcon } = useHeader();
+import not from '~/utils/not.jsx';
+
+const { isHomePage, mainIcon, headerText } = useHeader();
+
+/** CSS-классы для контента в шапке сайта */
+const headerContentClassObject = computed(() => {
+  return {
+    'header__content': true,
+    'header__content--home-page': isHomePage.value,
+  };
+});
 </script>
 
 <template>
   <header class="header">
-    <BaseIcon :icon-name="mainIcon.name" :width="mainIcon.width" :height="mainIcon.height" />
-    <span class="header__text header__text--uppercase">Книга контактов</span>
-    <BaseIcon
-      v-if="isCloseShown"
-      class="header__close-icon"
-      icon-name="close"
-      :width="24"
-      :height="24"
-    />
+    <div :class="headerContentClassObject">
+      <BaseIcon
+        v-if="mainIcon.isVisible"
+        :icon-name="mainIcon.name"
+        :width="mainIcon.width"
+        :height="mainIcon.height"
+      />
+      <span v-else class="header__first-letter-of-contact">{{ headerText[0] }}</span>
+      <span class="header__text">{{ headerText }}</span>
+    </div>
+    <BaseButton v-if="not(isHomePage)" class="header__close-button">
+      <BaseIcon icon-name="close" :width="24" :height="24" />
+    </BaseButton>
   </header>
 </template>
 
 <style lang="scss" scoped>
 .header {
+  position: relative;
   display: flex;
-  align-items: center;
-  column-gap: 4px;
   flex-flow: row nowrap;
   width: 100%;
   height: 40px;
   padding: 0 12px;
   background-color: #282828;
 
+  &__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    column-gap: 8px;
+    flex-flow: row nowrap;
+    margin: 0 auto;
+
+    &--home-page {
+      justify-content: flex-start;
+      column-gap: 4px;
+      margin: 0;
+      text-transform: uppercase;
+    }
+  }
+
+  &__first-letter-of-contact {
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    color: #545454;
+    border-radius: 50%;
+    background-color: #ffc700;
+    font-size: 10px;
+    font-weight: 700;
+    line-height: 20px;
+  }
+
   &__text {
     color: #dddddd;
     font-size: 14px;
     font-weight: 700;
     line-height: 1;
-
-    &--uppercase {
-      text-transform: uppercase;
-    }
   }
 
-  &__close-icon {
-    margin-left: auto;
+  &__close-button {
+    position: absolute;
+    top: 50%;
+    right: 12px;
+    padding: 0;
+    transform: translateY(-50%);
+    border: none;
+    background-color: transparent;
+
+    &:hover {
+      background-color: transparent;
+    }
   }
 }
 </style>
--- composable/header.ts
+++ composable/header.ts
@@ -1,32 +1,43 @@
 import { ref } from 'vue';
 
-/** Хук для управлени шапкой сайта */
+/** Хук для управления шапкой сайта */
 export default function useHeader() {
-  /** Показана ли кнопка "Закрыть" */
-  const isCloseShown = ref(true);
+  /** Открыта ли домашняя страница */
+  const isHomePage = ref(true);
+
+  /** Текст внутри шапки сайта */
+  const headerText = ref('Книга контактов');
+
+  /** Настройки основной иконки */
+  const mainIcon = ref({
+    isVisible: true,
+    name: 'contacts',
+    width: 31,
+    height: 24,
+  });
 
   /** Настройки роутинга */
   const route = useRoute();
 
-  /** Настройки основной иконки */
-  const mainIcon = computed(() => {
-    const mainIcon = {
-      name: 'contacts',
-      width: 31,
-      height: 24,
-    };
+  if (route.path !== '/') {
+    isHomePage.value = false;
+  }
 
-    return mainIcon;
-  });
+  if (route.path === '/edit') {
+    headerText.value = 'Двери' || 'Без имени';
+    mainIcon.value.isVisible = false;
+  }
 
-  onMounted(() => {
-    if (route.path === '/') {
-      isCloseShown.value = false;
-    }
-  });
+  if (route.path === '/new') {
+    headerText.value = 'Новый контакт';
+    mainIcon.value.name = 'new-user';
+    mainIcon.value.width = 16;
+    mainIcon.value.height = 16;
+  }
 
   return {
-    isCloseShown,
+    isHomePage,
+    headerText,
     mainIcon,
   };
 }
