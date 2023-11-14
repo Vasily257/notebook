b9d09a5a23c4363b82391e24c16d9f8ae6d7ba3a
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-14 19:44:51 +0500

    feat: add the BaseControl component

---
+++ components/BaseControl.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+/** Пропсы компонента */
+interface Props {
+  /** ID связанного контрола */
+  controlId: string;
+  /** Текста лейбла */
+  labelText: string;
+  /** Скрыть ли визуально лейбл */
+  isHidden?: boolean;
+}
+
+/** Пропсы со значениями по умолчанию */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<Props>(), {
+  isHidden: false,
+});
+</script>
+
+<template>
+  <p class="control">
+    <label :for="controlId" class="control__label">{{ labelText }}</label>
+    <slot></slot>
+  </p>
+</template>
+
+<style lang="scss" scoped>
+.control {
+  display: flex;
+  align-items: center;
+  column-gap: 12px;
+  flex-flow: row nowrap;
+  height: 40px;
+  margin: 0;
+  padding: 0;
+
+  &__label {
+    width: 72px;
+    color: #545454;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 1;
+
+    @media screen and (min-width: 450px) {
+      width: 136px;
+    }
+  }
+}
+
+</style>
