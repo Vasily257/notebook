7ac7c04ab51083581a51bb62c21c9c3fa9e594f2
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-15 07:41:35 +0500

    feat: stretch site to the whole page

--- app.vue
+++ app.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <TheHeader />
-    <main>
-      <NuxtPage />
-    </main>
-  </div>
+  <TheHeader />
+  <main class="main">
+    <NuxtPage />
+  </main>
 </template>
+
+<style lang="scss" scoped>
+.main {
+  min-height: calc(100vh - 40px);
+
+  @media screen and (min-width: 900px) {
+    min-height: calc(100vh - 48px);
+  }
+}
+</style>
