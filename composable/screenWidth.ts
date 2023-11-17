13cf2bca92b1f1ed8809bf96f9e2a4cfe24a086c
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-17 18:51:05 +0500

    refactor(useScreenWidth): use Bootstrap breakpoints

--- composable/screenWidth.ts
+++ composable/screenWidth.ts
@@ -5,29 +5,35 @@ import throttle from '~/utils/throttle';
 export default function useScreenWidth() {
   /** Значения брейкпоинтов */
   const breakpoints = {
-    small: 450,
-    medium: 600,
-    large: 900,
+    extraSmall: 376,
+    small: 576,
+    medium: 768,
+    large: 992,
     extraLarge: 1200,
+    extraExtraLarge: 1920,
   };
 
   /** Функция, которая реже вызывает обработчик изменения экрана */
   let throttledHandleResize = () => {};
 
-  const isExtraSmall = ref(true);
-  const isSmallAndWider = ref(false);
-  const isMediumAndWider = ref(false);
-  const isLargeAndWider = ref(false);
-  const isExtraLargeAndWider = ref(false);
+  const isExtraExtraSmall = ref(true);
+  const isExtraSmall = ref(false);
+  const isSmall = ref(false);
+  const isMedium = ref(false);
+  const isLarge = ref(false);
+  const isExtraLarge = ref(false);
+  const isExtraExtraLarge = ref(false);
 
   /** Обработать изменение экрана */
   const handleResize = () => {
     const width = window.innerWidth;
 
-    isSmallAndWider.value = width >= breakpoints.small;
-    isMediumAndWider.value = width >= breakpoints.medium;
-    isLargeAndWider.value = width >= breakpoints.large;
-    isExtraLargeAndWider.value = width >= breakpoints.extraLarge;
+    isExtraSmall.value = width >= breakpoints.extraSmall;
+    isSmall.value = width >= breakpoints.small;
+    isMedium.value = width >= breakpoints.medium;
+    isLarge.value = width >= breakpoints.large;
+    isExtraLarge.value = width >= breakpoints.extraLarge;
+    isExtraExtraLarge.value = width >= breakpoints.extraExtraLarge;
   };
 
   throttledHandleResize = throttle(handleResize, 150);
@@ -43,15 +49,19 @@ export default function useScreenWidth() {
   });
 
   return {
-    /** Меньше Small ширины */
+    /** Меньше 376px */
+    isExtraExtraSmall,
+    /** 376px и шире */
     isExtraSmall,
-    /** Small ширина и шире */
-    isSmallAndWider,
-    /** Medium ширина и шире */
-    isMediumAndWider,
-    /** Large ширина и шире */
-    isLargeAndWider,
-    /** Extra Large ширина и шире */
-    isExtraLargeAndWider,
+    /** 576px и шире */
+    isSmall,
+    /** 768px и шире */
+    isMedium,
+    /** 992px и шире */
+    isLarge,
+    /** 1200px и шире */
+    isExtraLarge,
+    /** 1920px и шире */
+    isExtraExtraLarge,
   };
 }
