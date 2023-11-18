b8978131a4f2b49311cdac283e7a780f7bc7345d
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-18 15:53:32 +0500

    fix(NewContact): delete excess wrapper

--- components/NewContact.vue
+++ components/NewContact.vue
@@ -3,9 +3,9 @@ import useForm from '~/composables/form';
 import not from '@/utils/not';
 
 const inputLabels = [
-  { name: 'name', text: 'Имя' },
-  { name: 'tel', text: 'Телефон' },
-  { name: 'email', text: 'E-mail' },
+  { name: 'name', text: 'Имя', placeholder: 'Например «Андрей»...' },
+  { name: 'tel', text: 'Телефон', placeholder: '+7(___)___-__-__' },
+  { name: 'email', text: 'E-mail', placeholder: 'Например «pochta@domain.ru»...' },
   { name: 'category', text: 'Категория' },
 ];
 
@@ -54,21 +54,24 @@ const form = reactive(useForm(fieldOptions));
   <div class="new-contact">
     <h2 class="new-contact__title">Новый контакт</h2>
     <ul class="new-contact__control-list">
-      <li v-for="(label, index) in inputLabels" :key="label.name" class="new-contact__control-item">
-        <BaseControl :control-id="label.name" :label-text="label.text">
-          <BaseInput
-            v-if="index < 3"
-            :id="label.name"
-            :name="label.name"
-            v-model="form.values[label.name]"
-          />
-          <BaseDropdown
-            v-if="index === 3"
-            :id="label.name"
-            :name="label.name"
-            :options="categoryOptions"
-          />
-        </BaseControl>
+      <li v-for="(label, index) in inputLabels" :key="label.name" class="new-contact__item">
+        <label :for="label.name" class="new-contact__label">{{ label.text }}</label>
+        <BaseInput
+          v-if="index < 3"
+          :id="label.name"
+          :name="label.name"
+          v-model="form.values[label.name]"
+          :placeholder="label.placeholder"
+          :label-text="label.text"
+          class="new-contact__input"
+        />
+        <BaseDropdown
+          v-if="index === 3"
+          :id="label.name"
+          :name="label.name"
+          :options="categoryOptions"
+          class="new-contact__dropdown"
+        />
       </li>
     </ul>
     <BaseButton class="new-contact__save-button" :disabled="not(form.isValid)">
@@ -104,14 +107,49 @@ const form = reactive(useForm(fieldOptions));
     list-style-type: none;
   }
 
-  &__control-item {
-    width: 100%;
+  &__item {
+    display: flex;
+    align-items: center;
+    flex-flow: row nowrap;
+    height: 40px;
+    margin: 0;
+    padding: 0;
+  }
+
+  &__label {
+    width: 72px;
+    margin-right: 12px;
+    color: #545454;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 1;
+
+    @media screen and (min-width: 576px) {
+      width: 136px;
+    }
+  }
+
+  &__input {
+    position: relative;
+    color: #545454;
+    font-size: 12px;
+    line-height: 1.6;
+
+    &::placeholder {
+      color: #a9a9a9;
+    }
+  }
+
+  &__dropdown {
+    font-size: 12px;
+    font-weight: 400;
   }
 
   &__save-button {
     margin-top: 32px;
     margin-left: 84px;
     background-color: #ffc700;
+    font-weight: 700;
 
     &:hover {
       background-color: #ffd84c;
