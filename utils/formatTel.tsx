58e7163d9219af798c1222411a9ce661414b37e8
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-23 09:59:43 +0500

    fix: add checking

--- utils/formatTel.tsx
+++ utils/formatTel.tsx
@@ -1,10 +1,16 @@
 /**
  * Получить отформатированный номер телефона
- * @param tel 10-значный номер телефона
+ * @param tel 11-значный номер телефона c «7» в начале
  * @returns телефон в формате +7(123)456-78-90
  */
 export default function formatTel(tel: string): string {
-  const [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11] = tel;
+  let result = '';
 
-  return `+${d1}(${d2}${d3}${d4})${d5}${d6}${d7}-${d8}${d9}-${d10}${d11}`;
+  if (tel?.length === 11) {
+    const [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11] = tel;
+
+    result = `+${d1}(${d2}${d3}${d4})${d5}${d6}${d7}-${d8}${d9}-${d10}${d11}`;
+  }
+
+  return result;
 }
