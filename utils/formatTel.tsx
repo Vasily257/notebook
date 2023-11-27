db12d9de3ff1483d5108cf260854113b0159ed33
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-11-27 15:42:43 +0500

    feat: add util to get a tel using mask

--- utils/formatTel.tsx
+++ utils/formatTel.tsx
@@ -1,3 +1,6 @@
+/** Маска телефонного номера */
+const matrix = '+7(___)___-__-__';
+
 /**
  * Получить номер телефона в полной форме
  * @param tel 11-значный номер телефона c «7» в начале
@@ -28,4 +31,42 @@ export function getShortTel(tel: string): string {
   }
 
   return result;
+}
+
+/**
+ * Получить номер телефона, используя заданную маску
+ * @param value текст, введенный в поле
+ * @returns телефон в формате +7(123)456-78-90
+ */
+export function getTelUsingMask(value: string): string {
+  /** Строка с числами */
+  let numberValues = value.replace(/\D/g, '');
+
+  /** Количество использованных чисел из numberValues */
+  let numberCounter = 0;
+
+  /** Строка с минимальным разрешенным количеством чисел */
+  const minNumbers = matrix.replace(/\D/g, '');
+
+  // Обработать вариант, когда пользователь ввел слишком короткий номер
+  if (minNumbers.length >= numberValues.length) numberValues = minNumbers;
+
+  /** Заменить символы в строке matrix в соответствии с введенными цифрами */
+  const replacer = (match: string) => {
+    if (/[0-9_]/.test(match) && numberCounter < numberValues.length) {
+      // Если символ является числом или нижнем подчеркиванием и если ещё не все числа перебраны,
+      // то увеличить счетчик и вернуть перебираемый символ из строки
+      numberCounter += 1;
+
+      return numberValues.charAt(numberCounter - 1);
+    } else if (numberCounter >= numberValues.length) {
+      // Если все цифры использованы, то не добавлять символы
+      return '';
+    } else {
+      return match;
+    }
+  };
+
+  // Вернуть обработанный текст
+  return matrix.replace(/./g, replacer);
 }
